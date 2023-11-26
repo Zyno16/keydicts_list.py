8886543b8f420894cd043de0a8956de94dadf05e
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-26 10:43:36 +0000

    Add files via upload

---
+++ keyDicts__list.py
@@ -0,0 +1,11 @@
+myD={"name":"zino","age":33,"city":"algiers","adult":True}
+print(myD.keys())
+#this will display the keys of dicts
+print(list(myD.keys()))
+#this is will display the list of keys
+print(list(myD.keys())[0])
+#this is will display first key on list
+print(list(myD.keys())[1])
+#this is will display second key on list
+
+
